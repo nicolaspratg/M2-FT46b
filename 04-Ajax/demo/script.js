f6fcbb2f51cb6ea26f434355c5d286d5f63eb3ed
nicolaspratg <nicoprat@hotmail.com>
2024-01-04 17:16:51 -0300

    Demo clase Jorge

---
+++ 04-Ajax/demo/script.js
@@ -0,0 +1,38 @@
+const URL_BASE = "https://jsonplaceholder.typicode.com";
+const usersList = document.querySelector("#usersList");
+
+const crateListItems = (user) => {
+  const li = document.createElement("li");
+  li.innerHTML = user.name;
+  usersList.appendChild(li);
+};
+
+const showUsers = () => {
+  $.get(`${URL_BASE}/users`, (response) => {
+    response.forEach(crateListItems);
+  });
+};
+
+$(".boton").click(showUsers);
+
+$(".delete").click(() => {
+  const firstUserListItem = $("#usersList li:first-child"); // Delete the first user each time the button is clicked
+
+  if (firstUserListItem.length) {
+    const userNameToDelete = firstUserListItem.text().trim(); // Get the user ID or name from the list item and use it in the DELETE request
+
+    $.ajax({
+      url: `${URL_BASE}/users/${userNameToDelete}`, // Adjust the URL based on your API
+      type: "DELETE",
+      success: (response) => {
+        console.log("User deleted successfully");
+        firstUserListItem.remove();
+      },
+      error: (error) => {
+        console.error("Error deleting user", error);
+      },
+    });
+  } else {
+    console.log("No users to delete");
+  }
+});
